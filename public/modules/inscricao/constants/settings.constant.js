6f05cb20f77036dfe8d4f8a908bb7becc07b8a7f
slacktracer <slacktracer@gmail.com>
2015-06-09 17:38:19 -0300

    Cálculo dos valores a pagar por etapa.

--- public/modules/inscricao/constants/settings.constant.js
+++ public/modules/inscricao/constants/settings.constant.js
@@ -4,10 +4,15 @@
         .module('inscricao')
         .constant('inscricao.settings', {
             'valores': {
-                '1': 30,
-                '2': 60,
-                '3': 80,
+                '1': 50,
+                '2': 80,
+                '3': 100,
                 'curso': 20
+            },
+            'categorias': {
+                '1': 'Estudante de Graduação',
+                '2': 'Estudante de Pós-Graduação ou Residente',
+                '3': 'Docente ou Profissional'
             },
             'cursos': {
                 'AMDI': 'Avaliação multidimensional do idoso',
@@ -16,6 +21,26 @@
                 'NEC': 'Nutrição e câncer',
                 'CCOPD': 'Cuidados com o paciente diabético',
                 'SNEEP': 'Suporte nutricional enteral e parenteral'
+            },
+            'valoresPorEtapa': {
+                'até 31/05': {
+                    '1': 30,
+                    '2': 60,
+                    '3': 80,
+                    'curso': 20
+                },
+                'até 31/07': {
+                    '1': 50,
+                    '2': 80,
+                    '3': 100,
+                    'curso': 20
+                },
+                'até o evento': {
+                    '1': 70,
+                    '2': 100,
+                    '3': 120,
+                    'curso': 20
+                }
             }
         });
 }());
--- public/modules/inscricao/controllers/confirm.controller.js
+++ public/modules/inscricao/controllers/confirm.controller.js
@@ -8,13 +8,15 @@
         '$scope',
         'inscricao.data',
         'inscricao.errorHandler',
-        'inscricao.notifications'
+        'inscricao.notifications',
+        'inscricao.settings'
     ];
     function Confirm(
         $scope,
         data,
         errorHandler,
-        notifications
+        notifications,
+        settings
     ) {
         var
             vm;
@@ -34,6 +36,28 @@
             jQuery('#confirmModal').on('show.bs.modal', function onShowBsModal() {
                 set();
             });
+        }
+        function calculate(categoria, cursoMatutino, cursoVespertino) {
+            var
+                totalPorEtapa;
+            totalPorEtapa = [];
+            totalPorEtapa[0] = settings.valoresPorEtapa['até 31/05'][categoria];
+            totalPorEtapa[1] = settings.valoresPorEtapa['até 31/07'][categoria];
+            totalPorEtapa[2] = settings.valoresPorEtapa['até o evento'][categoria];
+            if (cursoMatutino) {
+                totalPorEtapa[0] += settings.valoresPorEtapa['até 31/05'].curso;
+                totalPorEtapa[1] += settings.valoresPorEtapa['até 31/07'].curso;
+                totalPorEtapa[2] += settings.valoresPorEtapa['até o evento'].curso;
+            }
+            if (
+                cursoVespertino &&
+                cursoVespertino !== 'AMDI'
+            ) {
+                totalPorEtapa[0] += settings.valoresPorEtapa['até 31/05'].curso;
+                totalPorEtapa[1] += settings.valoresPorEtapa['até 31/07'].curso;
+                totalPorEtapa[2] += settings.valoresPorEtapa['até o evento'].curso;
+            }
+            return totalPorEtapa;
         }
         function confirm() {
             vm.afterInvalidSubmission = true;
@@ -77,6 +101,10 @@
         function set() {
             vm.inscricao = $scope.list.inscricoes.inscricao;
             vm.inscricao.copy = angular.copy(vm.inscricao);
+            vm.inscricao.copy.categoria = settings.categorias[vm.inscricao.copy.categoria];
+            vm.inscricao.copy.curso_matutino = settings.cursos[vm.inscricao.copy.curso_matutino];
+            vm.inscricao.copy.curso_vespertino = settings.cursos[vm.inscricao.copy.curso_vespertino];
+            vm.valoresPorEtapa = calculate(vm.inscricao.categoria, vm.inscricao.curso_matutino, vm.inscricao.curso_vespertino);
             $scope.$apply();
         }
     }
